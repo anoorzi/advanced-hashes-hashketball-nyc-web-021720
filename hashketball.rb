d5e917f7456c1defd7df96a4216f41f4640e01d2
Abubaker Noorzi <anoorzi123@gmail.com>
2020-02-14 15:54:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,52 @@
 # Write your code here!
-
+def game_hash
+  {
+    :away => { :team_name => "Charlette Hornets",
+                :colors => ["Turquoise","Purple"],
+                :players => [
+                  { :player_name => "Jeff Adrien",
+                    :number => 4,
+                    :shoe => 18,
+                    :points => 10,
+                    :rebounds => 1,
+                    :assists => 1,
+                    :steals => 2,
+                    :blocks => 7,
+                    :slam_dunks => 2
+                  },
+                  { :player_name => "Bismack Biyambo",
+                    :number => 0,
+                    :shoe => 16,
+                    :points => 12,
+                    :rebounds => 4,
+                    :assists => 7,
+                    :steals => 22,
+                    :blocks => 15,
+                    :slam_dunks => 10
+                  },
+                  { :player_name => "Baker noorzi",
+                    :number => 11,
+                    :shoe => 8,
+                    :points => 11,
+                    :rebounds => 4,
+                    :assists => 6,
+                    :steals => 5,
+                    :blocks => 7,
+                    :slam_dunks => 3
+                  },
+                  { :player_name => "Mr Smith",
+                    :number => 88,
+                    :shoe => 13,
+                    :points => 14,
+                    :rebounds => 15,
+                    :assists => 3,
+                    :steals => 2,
+                    :blocks => 5,
+                    :slam_dunks => 5
+                  },
+      
+    
+  
 
 
 
